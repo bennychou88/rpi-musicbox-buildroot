72254578ddde45e08632cf9ed47689739354bb0a
Jérémie Huchet <jeremiehuchet@users.noreply.github.com>
2016-03-11 00:54:49 +0100

    don't regenerate ssh keys if they exists

--- pre-image.sh
+++ pre-image.sh
@@ -4,7 +4,7 @@ target="$(dirname $0)/buildroot/output/target"
 
 mkdir -p $target/home/mpd
 
-ssh-keygen -q -f $target/etc/ssh/ssh_host_key -N '' -t rsa1
-ssh-keygen -f $target/etc/ssh/ssh_host_rsa_key -N '' -t rsa
-ssh-keygen -f $target/etc/ssh/ssh_host_dsa_key -N '' -t dsa
-ssh-keygen -f $target/etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
+test -f $target/etc/ssh/ssh_host_key      || ssh-keygen -q -f $target/etc/ssh/ssh_host_key -N '' -t rsa1
+test -f $target/etc/ssh/ssh_host_rsa_key  || ssh-keygen -f $target/etc/ssh/ssh_host_rsa_key -N '' -t rsa
+test -f $target/etc/ssh/ssh_host_dsa_key  || ssh-keygen -f $target/etc/ssh/ssh_host_dsa_key -N '' -t dsa
+test -f $target/etc/ssh/ssh_host_ecdsa_key|| ssh-keygen -f $target/etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
