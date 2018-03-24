a37f399327b185ff1c10454371efb95557f64a42
Jérémie Huchet <jeremiehuchet@users.noreply.github.com>
2018-03-24 18:55:27 +0100

    WIP enhancements

--- pre-image.sh
+++ pre-image.sh
@@ -14,3 +14,8 @@ test -f $target/etc/ssh/ssh_host_key      || ssh-keygen -q -f $target/etc/ssh/ss
 test -f $target/etc/ssh/ssh_host_rsa_key  || ssh-keygen -f $target/etc/ssh/ssh_host_rsa_key -N '' -t rsa
 test -f $target/etc/ssh/ssh_host_dsa_key  || ssh-keygen -f $target/etc/ssh/ssh_host_dsa_key -N '' -t dsa
 test -f $target/etc/ssh/ssh_host_ecdsa_key|| ssh-keygen -f $target/etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
+
+mkdir -p $target/root/.ssh
+test -f ~/.ssh/id_rsa.pub && cat ~/.ssh/id_rsa.pub > $target/root/.ssh/authorized_keys
+chmod 700 $target/root/.ssh
+chmod 600 $target/root/.ssh/authorized_keys
